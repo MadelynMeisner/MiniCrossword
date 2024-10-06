f2f887d2a928152afc15c42f10e0d4122d642abe
madelynmeisner <madelynmeisner@10.0.0.134>
2024-10-06 15:35:49 -0600

    recent

--- MiniCrossword/src/MiniCrossword.java
+++ MiniCrossword/src/MiniCrossword.java
@@ -1,70 +1,83 @@
-import java.util.ArrayList;
 import java.util.Scanner;
-//System.out.println();
+import java.util.ArrayList;
 public class MiniCrossword
 	{
-// blah blah blah
-		 static void main(String[] args)
-			{
-				//greetPlayer();
-				//displayBoard();
-				playerGuess();
-//			3 classes
-//				- this one
-//				- all questions
-//				- getters setters etc
-//				methods
-//				 intructions, greet, make sure each guess is how many letters 
-//				 enter which would print grid and questions , display
-//				method for player guess
-//				the board is out with questions 
-//				player says question number 1a ex and types the word,
-//				 if its right it fills in the board if its wrong it says 
-//				 try again, or try differetn questioh 
-//				method for 
-////				 
+//	player says question number 1a ex and types the word,
+//	and it fills out no matter wahter and displarys boarrd 
+//	again until its all filled out, once they have fileld 
+//	out out it will say i its corect or not. 
+//	 try again, or try differetn questioh 	
+//	** if i have time add a timer, and let the player redo crossword if the answer is wrong 
 
+	static void main(String[] args)
+	{
+	greetPlayer();
+	displayBoard();
+	playerGuess();
+	displayBoard();
+	checkAnswer();
+	}
+		 
+	public static void greetPlayer()
+	{
+		Scanner userInput = new Scanner (System.in);
+		System.out.println("Hello! Lets play a crossword!! The board is 5 x 5, which means each word should be five letters. To type your answer write the question number (ex. 1A or 1D) followed by your answer in all caps. Lets start, press enter to begin.  ");
+		String playerGuess = userInput.nextLine();
+	}
+	
+	public static void displayBoard()
+	{
+		String[][] boardMatrix = new String[5][5];
+		for (int row = 0; row < boardMatrix.length; row++)
+			{
+				for(int col = 0; col < boardMatrix.length; col ++)
+					{				
+					}
 			}
-		public static void greetPlayer()
+		System.out.println(Data.crosswordOne.get(0).getNumber.getQuestion + "     " + Data.crosswordTwo.get(0).getNumber.getQuestion);
+		System.out.println(Data.crosswordOne.get(1).getNumber.getQuestion + "     " + Data.crosswordTwo.get(1).getNumber.getQuestion);
+		System.out.println(Data.crosswordOne.get(2).getNumber.getQuestion + "     " + Data.crosswordTwo.get(2).getNumber.getQuestion);
+		System.out.println(Data.crosswordOne.get(3).getNumber.getQuestion + "     " + Data.crosswordTwo.get(3).getNumber.getQuestion);
+		System.out.println(Data.crosswordOne.get(4).getNumber.getQuestion + "     " + Data.crosswordTwo.get(4).getNumber.getQuestion);
+		System.out.println(  "    1     2   3   4   5");
+		System.out.println( "1 |_"+boardMatrix[0][0]+"_|_"+boardMatrix[0][1]+"_|_"+boardMatrix[0][2]+"_|_"+boardMatrix[0][3]+"_|_"+boardMatrix[0][4]+"_|");
+		System.out.println( "2 |_"+boardMatrix[1][0]+"_|_"+boardMatrix[1][1]+"_|_"+boardMatrix[1][2]+"_|_"+boardMatrix[1][3]+"_|_"+boardMatrix[1][4]+"_|");
+		System.out.println( "3 |_"+boardMatrix[2][0]+"_|_"+boardMatrix[2][1]+"_|_"+boardMatrix[2][2]+"_|_"+boardMatrix[2][3]+"_|_"+boardMatrix[2][4]+"_|");
+		System.out.println( "4 |_"+boardMatrix[3][0]+"_|_"+boardMatrix[3][1]+"_|_"+boardMatrix[3][2]+"_|_"+boardMatrix[3][3]+"_|_"+boardMatrix[3][4]+"_|");
+		System.out.println( "5 |_"+boardMatrix[4][0]+"_|_"+boardMatrix[4][1]+"_|_"+boardMatrix[4][2]+"_|_"+boardMatrix[4][3]+"_|_"+boardMatrix[4][4]+"_|");
+		
+	}
+	
+	public static void playerGuess()
+
+	{
+		//1A
+		if(playerGuess.equals(Data.crosswordOne.get(0).getAnswer()))
+	      {
+			boardMatrix[0][0] = "G";
+			boardMatrix[0][1] = "R";
+			boardMatrix[0][2] = "A";
+			boardMatrix[0][3] = "D";
+			boardMatrix[0][4] = "E";
+	      }
+
+		else if(!playerGuess.equals(Data.crosswordOne.get(0).getAnswer()))
+		         {
+			boardMatrix[0][0] = playerGuess.indexOf(0);
+			boardMatrix[0][1] = playerGuess.indexOf(1);
+			boardMatrix[0][2] = playerGuess.indexOf(2);
+			boardMatrix[0][3] = playerGuess.indexOf(3);
+			boardMatrix[0][4] = playerGuess.indexOf(4);
+		         }
+
+	}  
+	
+    public static void checkAnswer()
 		{
-			Scanner userInput = new Scanner (System.in);
-			System.out.println("Hello! Lets play a crossword!! The board is 5 x 5, which means each word should be five letters. To type your answer write the question number (ex. 1A) followed by your answer in all caps. Lets start, press enter to begin.  ");
-			String playerGuess = userInput.nextLine();
-		}
-		public static void displayBoard()
-		{
-			String[][] boardMatrix = new String[5][5];
-			for (int row = 0; row < boardMatrix.length; row++)
-				{
-					for(int col = 0; col < boardMatrix.length; col ++)
-						{				
-						}
-				}
-			System.out.println(  "    1     2   3   4   5");
-			System.out.println( "1 |_"+boardMatrix[0][0]+"_|_"+boardMatrix[0][1]+"_|_"+boardMatrix[0][2]+"_|_"+boardMatrix[0][3]+"_|_"+boardMatrix[0][4]+"_|");
-			System.out.println( "2 |_"+boardMatrix[1][0]+"_|_"+boardMatrix[1][1]+"_|_"+boardMatrix[1][2]+"_|_"+boardMatrix[1][3]+"_|_"+boardMatrix[1][4]+"_|");
-			System.out.println( "3 |_"+boardMatrix[2][0]+"_|_"+boardMatrix[2][1]+"_|_"+boardMatrix[2][2]+"_|_"+boardMatrix[2][3]+"_|_"+boardMatrix[2][4]+"_|");
-			System.out.println( "4 |_"+boardMatrix[3][0]+"_|_"+boardMatrix[3][1]+"_|_"+boardMatrix[3][2]+"_|_"+boardMatrix[3][3]+"_|_"+boardMatrix[3][4]+"_|");
-			System.out.println( "5 |_"+boardMatrix[4][0]+"_|_"+boardMatrix[4][1]+"_|_"+boardMatrix[4][2]+"_|_"+boardMatrix[4][3]+"_|_"+boardMatrix[4][4]+"_|");
 			
 		}
-		public static void playerGuess()
-		{
-			if(playerGuess.equals(Data.crossword.get(3).getAnswer()))
-			      {
-			        System.out.println("Great! thats the answer, 200 points will be added to your current score of " + playerScore + "!"); 
-			        System.out.println();
-			        System.out.println();
-			       
-			      }
-
-			      else if(!playerGuess.equals(Data.crossword.get(3).getAnswer()))
-			         {
-			           System.out.println("nope! the correct answer was " + Data.crossword.get(3).getAnswer() + ". No points will be added to your current score of " + playerScore +".");
-			           System.out.println();
-			         }
-			      
-			          values[0][5] = "  ";
-			    }    
-		}
+		
 	}
+	
+
+	
